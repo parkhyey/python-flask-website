001532f81bc2a01bd6cc09f813feed2fc1cb8d2e
Rina Park <parkhyey@oregonstate.edu>
2022-10-05 15:54:14 -0700

    minor text and css updates

--- static/nav.js
+++ static/nav.js
@@ -1,7 +1,7 @@
 document.getElementById("navbar").innerHTML =
     '<!-- navigation bar -->' +
     '<div class="nav">' +
-    '<button class="navbutton"><a href="signup.html">Sign Up</a></button>' +
-    '<button class="navbutton"><a href="login.html">Log In</a></button>' +
+    '<button class="navbutton"><a href="signup.html">SIGN UP</a></button>' +
+    '<button class="navbutton"><a href="login.html">SIGN IN</a></button>' +
     '</div>'
     ;
