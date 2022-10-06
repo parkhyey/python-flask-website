ee743f6e41d25e10fccb8ca4f5d0522c8d393927
Rina Park <parkhyey@oregonstate.edu>
2022-10-05 18:43:18 -0700

    created db_connector.py

---
+++ database/db_connector.py
@@ -0,0 +1,53 @@
+from flask import Flask
+import MySQLdb
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# Load our environment variables from the .env file in the root of our project.
+load_dotenv(find_dotenv())
+
+# Set the variables in our application with those environment variables
+host = os.environ.get("DBHOST")
+user = os.environ.get("DBUSER")
+passwd = os.environ.get("DBPW")
+db = os.environ.get("DB")
+
+def connect_to_database(host = host, user = user, passwd = passwd, db = db):
+    '''
+    connects to a database and returns a database objects
+    '''
+    db_connection = MySQLdb.connect(host,user,passwd,db)
+    db_connection.ping(True)
+    return db_connection
+
+def execute_query(db_connection = None, query = None, query_params = ()):
+    '''
+    executes a given SQL query on the given db connection and returns a Cursor object
+    db_connection: a MySQLdb connection object created by connect_to_database()
+    query: string containing SQL query
+    returns: A Cursor object as specified at https://www.python.org/dev/peps/pep-0249/#cursor-objects.
+    You need to run .fetchall() or .fetchone() on that object to actually acccess the results.
+    '''
+
+    if db_connection is None:
+        print("No connection to the database found! Have you called connect_to_database() first?")
+        return None
+
+    if query is None or len(query.strip()) == 0:
+        print("query is empty! Please pass a SQL query in query")
+        return None
+
+    print("Executing %s with %s" % (query, query_params));
+    # Create a cursor to execute query. Why? Because apparently they optimize execution by retaining a reference according to PEP0249
+    cursor = db_connection.cursor(MySQLdb.cursors.DictCursor)
+
+    '''
+    params = tuple()
+    #create a tuple of paramters to send with the query
+    for q in query_params:
+        params = params + (q)
+    '''
+    cursor.execute(query, query_params)
+    # this will actually commit any changes to the database. without this no changes will be committed!
+    db_connection.commit();
+    return cursor
