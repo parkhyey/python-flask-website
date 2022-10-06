32642c6ccec316ba923b36ab82fd2ab2564dc056
Rina Park <parkhyey@oregonstate.edu>
2022-10-05 19:41:44 -0700

    updated sample data

--- database/Profiles.sql
+++ database/Profiles.sql
@@ -5,12 +5,17 @@ DROP TABLE IF EXISTS Profiles;
 CREATE TABLE Profiles(
     profile_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     profile_name VARCHAR(255) NOT NULL,
-	profile_type VARCHAR(255) NOT NULL
+	profile_type VARCHAR(255) NOT NULL,
+	profile_breed VARCHAR(255) NOT NULL,
+	profile_disposition VARCHAR(255) NOT NULL,
+	profile_availability VARCHAR(255) NOT NULL,
+	profile_news VARCHAR(255) NOT NULL,
+	profile_description VARCHAR(255) NOT NULL
 ) ENGINE = InnoDB;
 
-INSERT INTO Profiles(profile_name, profile_type) 
+INSERT INTO Profiles(profile_name, profile_type, profile_breed, profile_disposition, profile_availability, profile_news, profile_description) 
 VALUES 
-    ("Layla", "cat"),
-    ("March", "dog"),
-    ("Ram", "other")
+    ("Layla", "Cat", "Siamese", "Good with other animals", "Available", "Newly added!", "Small size, long tail, white with brown paws"),
+    ("March", "Dog", "Golden Retriever", "Good with children", "Available", "Newly added!", "Large size, light golden fur"),
+    ("Ram", "Other", "N/A", "Good with children", "Available", "Newly added!", "Small size, black goat")
 ;
